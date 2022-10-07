9f7a76003bad8ba5c5d70134d1a74f2a2d503bec
chris1246 <christoffer.gamel@gmail.com>
2022-10-07 23:32:05 +0200

    Added Ram and CPU usage monitoring

--- vitals.py
+++ vitals.py
@@ -1,15 +1,12 @@
 from gpiozero import CPUTemperature, DiskUsage
 import shutil
+import psutil
 
 cpu = CPUTemperature()
 disk = DiskUsage()
 path = "/media/pi/Sanne Data"
 stat = shutil.disk_usage(path)
+calc = ((stat[1] / stat[0]) * 100)
 
-
-print(f'CPU Temperature: {cpu.temperature}')
-print(f'Internal disk usage: {disk.usage}%')
-calc = (stat[1] / stat[0]) * 100
-print(f"External disk usage: {calc}%")
-
-#Ping server
+while True:
+    print(f'CPU Temp: {cpu.temperature} \t CPU {psutil.cpu_percent(4)}% \t SD%: {disk.usage}% \t SSD: {calc}%, Memory used: {psutil.virtual_memory().percent}%')
