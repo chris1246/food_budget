93d29ff45c4acb538b4fc39e07785c59b26ec958
chris1246 <christoffer.gamel@gmail.com>
2022-10-07 12:31:41 +0200

    show columns for meals

--- mapper.py
+++ mapper.py
@@ -7,7 +7,7 @@ import mysql.connector as mariadb
 mariadb_connection = mariadb.connect(user='local2', password='0000', database='home_assistant', host='10.209.154.141', port='3306')
 create_cursor = mariadb_connection.cursor()
 
-#create_cursor.execute("SHOW COLUMNS FROM meals;")
-create_cursor.execute("SHOW TABLES;")
+create_cursor.execute("SHOW COLUMNS FROM Meals;")
+#create_cursor.execute("SHOW TABLES;")
 for x in create_cursor:
     print(x)
