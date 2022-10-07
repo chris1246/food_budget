46abb4314722de82d9b03e11c2229f49c99fff1f
chris1246 <christoffer.gamel@gmail.com>
2022-10-07 14:54:01 +0200

    monitoring for external SSD

--- temp.py
+++ temp.py
@@ -1,6 +1,15 @@
 from gpiozero import CPUTemperature, DiskUsage
+import shutil
+
 cpu = CPUTemperature()
 disk = DiskUsage()
+path = "/media/pi/Sanne Data"
+stat = shutil.disk_usage(path)
+
 
 print(f'CPU Temperature: {cpu.temperature}')
 print(f'Current disk usage: {disk.usage}%')
+
+print(f"Disk usage statistics:{stat}")
+
+#Ping server
