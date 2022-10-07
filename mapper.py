a73d82b97e639196d93ed0154a20929f79f43efe
chris1246 <christoffer.gamel@gmail.com>
2022-10-07 12:27:48 +0200

    show columns from table

--- mapper.py
+++ mapper.py
@@ -7,7 +7,7 @@ import mysql.connector as mariadb
 mariadb_connection = mariadb.connect(user='local2', password='0000', database='home_assistant', host='10.209.154.141', port='3306')
 create_cursor = mariadb_connection.cursor()
 
-create_cursor.execute("SHOW TABLES")
+create_cursor.execute("SHOW COLUMNS FROM python_creation_table;")
 
 for x in create_cursor:
     print(x)
