4c83dccb72102eded466bdf28f6eaade861d9667
chris1246 <christoffer.gamel@gmail.com>
2022-10-08 14:36:01 +0200

    login through json

--- mapper.py
+++ mapper.py
@@ -1,7 +1,20 @@
 # File: mapper.py     Function: not in use   Type: script
 import mysql.connector as mariadb
-mariadb_connection = mariadb.connect(user='local2', password='0000', database='home_assistant', host='10.209.154.141', port='3306')
-create_cursor = mariadb_connection.cursor()
+
+
+a = ['user', 'password', 'database', 'host', 'port']
+credentials = []
+with open('connection_parameters.json') as f:
+    data = json.load(f)
+for i in range(len(data)):
+    credentials.append(data[a[i]])
+
+mariadb_connection = mariadb.connect(user=f'{credentials[0]}', 
+                            password=f'{credentials[1]}', 
+                            database=f'{credentials[2]}', 
+                            host=f'{credentials[3]}', 
+                            port=f'{credentials[4]}')
+create_cursor = self.mariadb_connection.cursor()
 create_cursor.execute("SHOW COLUMNS FROM Meals;")
 #create_cursor.execute("SHOW TABLES;")
 for x in create_cursor:
