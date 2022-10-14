e16d8cf3501679c9902f7ab936c51ecd6b9c8edd
chris1246 <christoffer.gamel@gmail.com>
2022-10-14 17:58:13 +0100

    filter adjustments

--- py_scripts/receipt_data.py
+++ py_scripts/receipt_data.py
@@ -23,8 +23,8 @@ class reader():
         self.pageContent = page.extractText()
         os.remove(f"{path}/{insertion_data}")
         self.lines = self.pageContent.split('\n')
-        #for i in range(len(self.lines)):
-        #   print(self.lines[i])
+        for t in range(len(self.lines)):
+            print(f"Line: {t}: {self.lines[t]}")
         self.status = 0
         self.identifier()
         if(len(self.items_found) > 0):
@@ -36,22 +36,23 @@ class reader():
 
     def identifier(self):
         txt_line = self.lines
+        
         self.items_found = []
         if self.status == 0:
             keyword = list(self.search_parameters['stores'])
             for line in range(len(txt_line)):                   
                 for keywords in range(len(keyword)):
                     initiate = re.search(keyword[keywords].lower(), txt_line[line].lower())
-                    print(f"Searched: {txt_line[line].lower()} for {keyword[keywords]}")
+                    #print(f"Searched: {txt_line[line].lower()} for {keyword[keywords]}")
                     if initiate:
                         self.items_found.append(keyword[keywords])
-                        print(f"Keyword {keyword[keywords]}, found on line {line}, with text: '{txt_line[line]}'")
+                        #print(f"Keyword {keyword[keywords]}, found on line {line}, with text: '{txt_line[line]}'")
                         self.item = keyword[keywords]
         else:
             keyword = self.search_list[0]
             for line in range(len(txt_line)):
                 initiate = re.search(keyword.lower(), txt_line[line].lower())
-                print(f"Searched: {txt_line[line].lower()} for {keyword.lower()}")
+                #print(f"Searched: {txt_line[line].lower()} for {keyword.lower()}")
                 if initiate:
                     self.items_found.append(keyword)
                     self.on_line.append(line)
@@ -85,22 +86,17 @@ class reader():
 
             self.store_name = store_name
 
-            self.parameters_creator()
-
-
-            
+            self.parameters_creator()     
         elif(len(self.items_found) == 0):
             print(f"No examples found with: {store_name}, try again")
             
             self.store_creator()
 
-
-
     def parameters_creator(self):
-        indexes = ['Input name of store: ','Enter the name of the first item: ', 'Enter the name of the last item: ']
+        indexes = ['Input name of store: ','Enter the name of the first item: ','Enter quantity of first item: ','Enter price of first item: ','Enter the name of the last item: ']
         self.parameters = []
-        
-        for i in range(3):
+        txt_line = self.lines
+        for i in range(len(indexes)):
             initial_search = input(f"{indexes[i]}")
             self.search_list.append(initial_search)
             self.status = 1
@@ -108,21 +104,17 @@ class reader():
             self.search_list.clear()
             print(self.items_found)
             if(len(self.items_found) > 0):
-                print(f"Found {len(self.items_found)} examples of {initial_search}")
+                #print(f"Found {len(self.items_found)} examples of {initial_search}")
+                if i == 1:
+                    #index_range = len[self.on_line]
+                    line_srch = self.on_line[len(self.on_line)-1]
+                    
+                if i == 2:
 
-                #current_keywords = self.search_parameters['stores']
-                #print(current_keywords)
-                #current_keywords.append(f"{initial_search}")
-                #print(current_keywords)
-                #print(json.dumps(self.search_parameters))
-                #json.dump(current_keywords, self.json_file)
-                #with open(self.json_file, 'w') as out_file:
-                #    out = json.dumps(self.search_parameters)
-                #    out_file.write(out)
+                    srch_proximity = re.search(initial_search.lower(), txt_line[line_srch].lower())
+                    if srch_proximity:
+                        print(f"Found {initial_search} on same line as first item")
                 self.parameters.append(initial_search)
-                #self.store_name = store_name
-                #self.start
-
             elif(len(self.items_found) == 0):
                 print(f"No examples found with: {initial_search}, try again")
                 
@@ -130,6 +122,23 @@ class reader():
             
                 #enter first item
                 #enter last item
+        start_line = self.on_line[1]
+        print(f"start_line{start_line}")
+        end_line = self.on_line[4]
+        print(f"end_line{end_line}")
+
         print(self.parameters)
+        current_keywords = self.search_parameters['stores']
+        print(current_keywords)
+        print(self.on_line)
+        if start_line > end_line:
+            print("Error, Last item initiated before first item")
+            self.parameters_creator()
+
+        
+        
+        
+        
+        
         
         
