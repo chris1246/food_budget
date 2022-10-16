0bf96533900a2b50e6ad8e91067fa5327208bb1a
Christoffer Gamél <98660011+chris1246@users.noreply.github.com>
2022-10-16 18:15:23 +0200

    maybe?maybe

--- py_scripts/receipt_data.py
+++ py_scripts/receipt_data.py
@@ -11,6 +11,7 @@ class reader():
             self.search_parameters = json.load(f)
         self.search_list = []
         self.on_line = []
+        self.words = []
         
         
 
@@ -23,7 +24,14 @@ class reader():
         self.pageContent = page.extractText()
         os.remove(f"{path}/{insertion_data}")
         self.lines = self.pageContent.split('\n')
+<<<<<<< HEAD
+
+        for lines in range(len(self.lines)):
+
+            self.words.append(self.lines[lines].split(' '))
+=======
         self.words = self.lines.split(' ')
+>>>>>>> 67d5e5f48daad3f42130d3f129284f234efb1549
         print(self.words)
         for t in range(len(self.lines)):
             print(f"Line: {t}: {self.lines[t]}")
